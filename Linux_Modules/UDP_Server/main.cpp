99ec73aedf0fd2ab9752afb9a57e08b93422dfc2
cmgrah <c.m.graham@live.com.au>
2016-12-13 18:35:07 +1100

    Simple packet object test
    Basic test to create a socket object using <sys/socket.h>. Compiles on
    netbook using g++, but VS on windows spits out errors.

--- Linux_Modules/UDP_Server/main.cpp
+++ Linux_Modules/UDP_Server/main.cpp
@@ -1,7 +1,15 @@
 #include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <sys/types.h> 
+#include <sys/socket.h>
 
 int main()
 {
     printf("hello from UDP_Server!\n");
+	//Just going to try out sending some UDP packets
+	//Tutorial: https://www.cs.rutgers.edu/~pxk/417/notes/sockets/udp.html
+
+	int s = socket(AF_INET, SOCK_DGRAM, 0);
     return 0;
 }
