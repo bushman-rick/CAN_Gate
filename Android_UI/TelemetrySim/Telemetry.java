a17ad110d900ecd65de1aabba3c7ec91e4f013ad
Simon Martin <sl.martin351@gmail.com>
2017-01-12 22:00:44 +1100

    Updated Telemetry.java
    New functionality for char arrays

--- Android_UI/TelemetrySim/Telemetry.java
+++ Android_UI/TelemetrySim/Telemetry.java
@@ -8,6 +8,9 @@ import android.widget.TextView;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Telemetry extends AppCompatActivity {
 
@@ -34,16 +37,29 @@ public class Telemetry extends AppCompatActivity {
         myUdpServer.kill();
     }
 
-
     private class UdpServer extends Thread {
 
         private boolean running = true;
         private String lastMessage = "";
+        private char[] headingArr;
+        private char[] accelArr;
+        private char[] trackArr;
+        private char[] coordXArr;
+        private char[] coordYArr;
+        private String testT = "";
+        private String conv = "";
+        private float track;
+        //test strings
+        private StringBuilder hex = new StringBuilder();
+        private String hexC;
+
 
 
         public void run() {
             int udp_port = 8583;
             String message;
+            char[] testA;
+
             byte[] lmessage = new byte[2048];
 
             DatagramPacket packet = new DatagramPacket(lmessage, lmessage.length);
@@ -55,8 +71,44 @@ public class Telemetry extends AppCompatActivity {
                     dsocket.receive(packet);
                     message = new String(lmessage, 0, packet.getLength());
                     lastMessage = message;
+                    // take input and turn it into a char array
+                    testA = message.toCharArray();
+                    headingArr = Arrays.copyOfRange(testA,0,4);
+                    accelArr = Arrays.copyOfRange(testA,4,8);
+                    // ...
+                    // copy the relevant section of the char array into the track array
+                    trackArr = Arrays.copyOfRange(testA,8,12);
+                    coordXArr = Arrays.copyOfRange(testA,12,16);
+                    coordYArr = Arrays.copyOfRange(testA,16,20);
+
+                    //testT = String.valueOf(trackArr);
+
+                    // This is where I need some help...
+                    // track is a float declared earlier and it is possible to cast char into a
+                    // float without explicitly declaring it cus a char requires more bytes.
+                    // This will display a number that has come from hex but I need the whole hex
+                    // in the array in order to display the correct number.
+                    track = trackArr[1];
+                    //track = Float.parseFloat(testT);
+
+
+                        for (int i=0; i<headingArr.length; i++) {
+                            //track = trackArr[i];
+
+                            hex.append(String.format("%04x", (int) headingArr[i]));
+                            hex.append(String.format("%04x", (int) accelArr[i]));
+                            hex.append(String.format("%04x", (int) trackArr[i]));
+                            hex.append(String.format("%04x", (int) coordXArr[i]));
+                            hex.append(String.format("%04x", (int) coordYArr[i]));
+                            hexC = hex.toString();
+
+
+                        }
+
                     runOnUiThread(updateTextMessage);
-                    Log.i("UDP packet received", message);
+                    //Log.i("UDP packet received", Arrays.toString(testT));
+                    //Log.i("UDP packet received", f);
+                    Log.i("UDP packet received", Float.toString(track));
                 }
             } catch (Throwable e) {
                 e.printStackTrace();
@@ -70,6 +122,10 @@ public class Telemetry extends AppCompatActivity {
 
         public String getLastMessage() {
             return lastMessage;
+        }
+
+        public float getTheDigits() {
+            return track;
         }
 
     }
